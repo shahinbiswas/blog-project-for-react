d3857b525aba3b0a2654caabaf7c8a69c0ad35c9
shahinbiswas <shahinulislam8397@gmail>
2024-11-27 19:55:00 +0600

    initial commit for react project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Home from "./components/STATE_LIFTING/Home";
+
+// import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
